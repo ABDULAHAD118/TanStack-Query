7b63b2e70ba7883eb9a0738836fc1ec3e22b573f
ABDULAHAD <129290085+ABDULAHAD118@users.noreply.github.com>
2025-01-06 21:21:26 +0500

    Adding Navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,21 @@
+import React from "react";
+import { Routes, Route } from "react-router-dom";
 import Products from "./Products";
-import React from 'react'
+import Product from "./Product";
+import Navbar from "./Navbar";
 
 export default function App() {
   return (
     <>
-    <h1 className="text-3xl p-10 font-bold flex justify-center items-center">
-      TanStack React Query
-    </h1>
-     <Products />
+      <Navbar />
+      <h1 className="text-3xl p-10 font-bold flex justify-center items-center">
+        TanStack React Query
+      </h1>
+      <Routes>
+        {/* Route for the home page (Products List) */}
+        <Route path="/" element={<Products />} />
+        <Route path="/product/:id" element={<Product />} />
+      </Routes>
     </>
-  )
+  );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,28 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Product from './Product.jsx'
-import { createBrowserRouter, RouterProvider } from 'react-router'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import { BrowserRouter } from 'react-router'
 
-const Router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-  },
-  {
-    path: '/product/:id',
-    element: <Product />,
-  }
-])
 
 const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={Router} />
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
       <ReactQueryDevtools buttonPosition='bottom-left' initialIsOpen={false} />
     </QueryClientProvider>
   </StrictMode>,
